38831f0db7c3ee3a8ab0ae00de7e729828ad2a2d
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-05-08 11:25:16 -0400

    First Commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/food', foodRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log("connected to db & listening on port", process.env.PORT)
         }) 
     })

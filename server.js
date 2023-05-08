53d0f73aa93bec811622c08d8720f4a53c610fac
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-05-08 12:24:15 -0400

    cors issue

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const foodRoutes = require('./routes/food')
 
 const app = express()
+
+app.use(cors({
+    origin: 'https://fit-fuel.netlify.app/'
+}))
 
 app.use(express.json())
 
